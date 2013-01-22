f16ad30f72ec1136b943a988c8ed19f1ede20e56
John Cowie <john.a.cowie@gmail.com>
2013-01-22 17:31:13 +0000

    twitter properties file is passed in as command line argument

--- src/nuotl_twitter/core.clj
+++ src/nuotl_twitter/core.clj
@@ -2,16 +2,14 @@
   (:require [cheshire.core :as j])
   (:gen-class))
 
-(def credentials (clojure.java.io/input-stream (clojure.java.io/resource "twitter4j.properties")))
-
-(def configuration (twitter4j.conf.PropertyConfiguration. credentials))
+(defn configuration [file]
+  (twitter4j.conf.PropertyConfiguration. (clojure.java.io/input-stream file)))
 
 (defn json-parser [json] (println json) (println (:id json)))
-
 (def listener (ClojureStatusListener. json-parser #(println %) #(println %)))
 
 (defn -main [& args]
   (let [stream
-        (. (twitter4j.TwitterStreamFactory. configuration) (getInstance))]
+        (. (twitter4j.TwitterStreamFactory. (configuration (nth args 0))) (getInstance))]
   (. stream (addListener listener))
   (. stream (user))))
