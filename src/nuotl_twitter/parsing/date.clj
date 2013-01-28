0a1acfbc0b26157e0613148904675e86a5f3fc0d
John Cowie <john.a.cowie@gmail.com>
2013-01-28 23:29:35 +0000

    Added support for today and tomorrow strings in parser

--- src/nuotl_twitter/parsing/date.clj
+++ src/nuotl_twitter/parsing/date.clj
@@ -1,7 +1,28 @@
 (ns nuotl-twitter.parsing.date
-  (:require [clj-time.format :as f]))
+  (:require [clj-time.format :as f]
+            [clj-time.core :as t]
+            [clojure.string :as s]
+            ))
 
-(defn parse-date [string]
-  (try
-    (f/parse (f/formatter "dd/MM/yyyy") string)
-    (catch Exception e nil)))
+(defn today [] (t/now))
+
+(defn tomorrow []
+  (t/plus (today) (t/days 1)))
+
+(defn day-id {
+              "monday" 1
+              "tuesday" 2
+              "wednesday" 3
+              "thursday" 4
+              "friday" 5
+              "saturday" 6
+              "sunday" 7
+              })
+
+(defn parse-date [val]
+  (case (s/lower-case val)
+    ("today" "2day") (today)
+    ("tomorrow" "2moro" "2morro" "tomoro" "2morrow" "2morow") (tomorrow)
+    (try
+      (f/parse (f/formatter "dd/MM/yyyy") val)
+      (catch Exception e nil))))
--- test/nuotl_twitter/parsing/date_test.clj
+++ test/nuotl_twitter/parsing/date_test.clj
@@ -13,7 +13,10 @@
         )))
 
 (facts
+ (against-background (t/now) => (t/date-time 2013 1 28 7 0 0))
  (parser/parse-date "lkjlsdf") => nil
  (parser/parse-date "25/12/2012") => (matches-date? 2012 12 25)
  (parser/parse-date "2/1/2013") => (matches-date? 2013 1 2)
+ (parser/parse-date "today") => (matches-date? 2013 1 28)
+ (parser/parse-date "tomorrow") => (matches-date? 2013 1 29)
  )
