bde63928bf298721825cdc5ab9c153662f3c4bc3
John Cowie <john.a.cowie@gmail.com>
2013-02-10 17:28:20 +0000

    Failing test for success url

--- test/nuotl_twitter/responder_test.clj
+++ test/nuotl_twitter/responder_test.clj
@@ -1,19 +1,20 @@
 (ns nuotl-twitter.responder-test
   (:require [nuotl-twitter.responder :as r]
-            [nuotl-twitter.messages :as m])
+            [nuotl-twitter.messages :as m]
+            [clj-time.core :as t])
   (:use [midje.sweet])
   )
 
-(defn reply-fn [tweet-id message]
+(defn test-reply-fn [tweet-id message]
   {tweet-id message})
 
 (defn create-tweet [tweet-id user-name user-id]
-  {:_id tweet-id :tweeter {:_id user-id :name user-name}})
+  {:_id tweet-id :start (t/date-time 2013 3 2) :tweeter {:_id user-id :name user-name}})
 
 (facts
  (against-background (m/get-message :success) => "SUCCESS")
- (r/respond reply-fn (create-tweet 77 "derek" 44) :success)
- => {77 "@derek SUCCESS http://twitter.com/44/status/77"}
- (r/respond reply-fn (create-tweet 89 "bobgeldof" 61) :is-me)
+ (r/respond test-reply-fn (create-tweet 77 "derek" 44) :success)
+ => {77 "@derek SUCCESS http://nextupontheleft.org/events/3/2013#77"}
+ (r/respond test-reply-fn (create-tweet 89 "bobgeldof" 61) :is-me)
  => nil
  )
